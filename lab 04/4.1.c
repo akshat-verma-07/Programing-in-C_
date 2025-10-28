c773dbd63ec6e3fa3b5eaa83d6a3ff93b78a8d93
Akshat Verma <akshat157verma@gmail.com>
2025-10-28 16:37:01 +0530

    Create 4.1.c

---
+++ lab 04/4.1.c
@@ -0,0 +1,25 @@
+/*3.4. WAP to check if three points (x1,y1), (x2,y2) and (x3,y3) are collinear or 
+not. */
+ 
+#include <stdio.h> 
+int main(){ 
+    int x1,y1,x2,y2,x3,y3; 
+ 
+    printf("enter first pair of coordinates:"); 
+    scanf("%d %d",&x1,&y1); 
+ 
+    printf("enter second pair of coordinates:"); 
+    scanf("%d %d",&x2,&y2); 
+ 
+    printf("enter third pair of coordinates:"); 
+    scanf("%d %d",&x3,&y3); 
+ 
+    if(x1*(y2-y3) + x2*(y3-y1) + x3*(y1-y2)==0){ 
+        printf("The given points are collinear"); 
+    } 
+ 
+    else{ 
+        printf("The given points are not collinear"); 
+    } 
+    return 0; 
+} 
