84b0c3b67a8114193bba09004dd804b9b054b86d
Akshat Verma <akshat157verma@gmail.com>
2025-10-28 16:51:56 +0530

    Create 6.6.c

---
+++ lab 06/6.6.c
@@ -0,0 +1,13 @@
+/* 4.3.6 */
+#include <stdio.h> 
+int main() { 
+printf ("Name - AKSHAT VERMA\nSAP_ID-590026318\nCourse-BCA\nBatch-6"); 
+printf("\n--------------------------------------\n"); 
+for (int i = 5; i >= 1; i--) { 
+for (int j = i; j >= 1; j--) { 
+printf("%d",j);  
+} 
+printf("\n");  
+} 
+return 0; 
+}
