354f3a54afcab488f6b397b242f2a71c9a203827
Akshat Verma <akshat157verma@gmail.com>
2025-10-28 17:33:28 +0530

    Create 14.c

---
+++ 100 Days of code/11 - 20/14.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    printf("Name - AKSHAT VERMA\nSAP_ID-590026318\nCourse-BCA\nBatch-6");
+    printf("\n--------------------------------------\n");
+
+    char ch;
+    scanf(" %c", &ch);
+    if (ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||
+        ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+        printf("Vowel");
+    else
+        printf("Consonant");
+    return 0;
+}
