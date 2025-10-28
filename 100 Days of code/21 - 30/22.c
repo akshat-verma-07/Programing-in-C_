fedf9b4c9052219390b6f320f084b920b95edd9c
Akshat Verma <akshat157verma@gmail.com>
2025-10-28 17:39:48 +0530

    Update 22.c

--- 100 Days of code/21 - 30/22.c
+++ 100 Days of code/21 - 30/22.c
@@ -4,14 +4,17 @@ int main() {
     printf("Name - AKSHAT VERMA\nSAP_ID-590026318\nCourse-BCA\nBatch-6");
     printf("\n--------------------------------------\n");
 
-    int month;
-    scanf("%d", &month);
-    switch (month) {
-        case 1: printf("January, 31 days"); break;
-        case 2: printf("February, 28 days"); break;
-        case 3: printf("March, 31 days"); break;
-        case 4: printf("April, 30 days"); break;
-        case 5: printf("May, 31 days"); break;
-        case 6: printf("June, 30 days"); break;
-        case 7: printf("July, 31 days"); break;
-        case
+    float cost, sell, percent;
+    scanf("%f %f", &cost, &sell);
+
+    if (sell > cost) {
+        percent = ((sell - cost) / cost) * 100;
+        printf("Profit %.0f%%", percent);
+    } else if (sell < cost) {
+        percent = ((cost - sell) / cost) * 100;
+        printf("Loss %.0f%%", percent);
+    } else {
+        printf("No Profit No Loss");
+    }
+    return 0;
+}
