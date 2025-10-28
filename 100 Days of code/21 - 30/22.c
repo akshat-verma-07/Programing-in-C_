be5eac26b077a6edf0e2834d2c23bc11a72ffbe0
Akshat Verma <akshat157verma@gmail.com>
2025-10-28 17:38:57 +0530

    Create 22.c

---
+++ 100 Days of code/21 - 30/22.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    printf("Name - AKSHAT VERMA\nSAP_ID-590026318\nCourse-BCA\nBatch-6");
+    printf("\n--------------------------------------\n");
+
+    int month;
+    scanf("%d", &month);
+    switch (month) {
+        case 1: printf("January, 31 days"); break;
+        case 2: printf("February, 28 days"); break;
+        case 3: printf("March, 31 days"); break;
+        case 4: printf("April, 30 days"); break;
+        case 5: printf("May, 31 days"); break;
+        case 6: printf("June, 30 days"); break;
+        case 7: printf("July, 31 days"); break;
+        case
