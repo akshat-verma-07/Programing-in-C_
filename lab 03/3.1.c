a23692535254df1d9174c4c09c50679d345542c8
Akshat Verma <akshat157verma@gmail.com>
2025-10-28 16:32:03 +0530

    Create 3.1.c

---
+++ lab 03/3.1.c
@@ -0,0 +1,15 @@
+/*3.1.⁠ ⁠ Write a C program to check whether a number is Even or ODD */
+#include <stdio.h> 
+int main(){ 
+int x; 
+printf("enter number to be checked"); 
+scanf("%d",&x); 
+if (x%2==0){ 
+printf("the number is even"); 
+} 
+else 
+{ 
+} 
+printf("the number is odd"); 
+return 0; 
+}
