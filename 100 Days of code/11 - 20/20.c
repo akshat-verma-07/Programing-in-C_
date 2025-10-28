02ded51a2105a9e16c0c09b9a2d5c001291c8ff8
Akshat Verma <akshat157verma@gmail.com>
2025-10-28 17:36:23 +0530

    Create 20.c

---
+++ 100 Days of code/11 - 20/20.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    printf("Name - AKSHAT VERMA\nSAP_ID-590026318\nCourse-BCA\nBatch-6");
+    printf("\n--------------------------------------\n");
+
+    int day;
+    scanf("%d", &day);
+    switch (day) {
+        case 1: printf("Monday"); break;
+        case 2: printf("Tuesday"); break;
+        case 3: printf("Wednesday"); break;
+        case 4: printf("Thursday"); break;
+        case 5: printf("Friday"); break;
+        case 6: printf("Saturday"); break;
+        case 7: printf("Sunday"); break;
+        default: printf("Invalid input");
+    }
+    return 0;
+}
