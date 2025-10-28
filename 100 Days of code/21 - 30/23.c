9743bad1b0dfdbd5d8cc9bec715dc631fcb3c14d
Akshat Verma <akshat157verma@gmail.com>
2025-10-28 17:39:21 +0530

    Create 23.c

---
+++ 100 Days of code/21 - 30/23.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    printf("Name - AKSHAT VERMA\nSAP_ID-590026318\nCourse-BCA\nBatch-6");
+    printf("\n--------------------------------------\n");
+
+    int days, fine = 0;
+    scanf("%d", &days);
+
+    if (days <= 5)
+        fine = days * 2;
+    else if (days <= 10)
+        fine = (5 * 2) + (days - 5) * 4;
+    else if (days <= 30)
+        fine = (5 * 2) + (5 * 4) + (days - 10) * 6;
+    else {
+        printf("Membership Cancelled");
+        return 0;
+    }
+
+    printf("Fine ₹%d", fine);
+    return 0;
+}
