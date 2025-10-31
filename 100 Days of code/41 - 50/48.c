e16ac489758cbc35b1d8d2e88cc34a4053d35656
Akshat Verma <akshat157verma@gmail.com>
2025-10-31 12:23:30 +0530

    Create 48.c

---
+++ 100 Days of code/41 - 50/48.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    printf("Name - AKSHAT VERMA\nSAP_ID-590026318\nCourse-BCA\nBatch-6");
+    printf("\n--------------------------------------\n");
+
+    for(int i = 1; i <= 5; i++) {
+        for(int j = 1; j <= i; j++)
+            printf("%d", j);
+        printf("\n");
+    }
+    return 0;
+}
