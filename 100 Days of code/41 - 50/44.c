9ea9c03ee1174a0450870c5d1970df4660f348da
Akshat Verma <akshat157verma@gmail.com>
2025-10-31 12:21:36 +0530

    Create 44.c

---
+++ 100 Days of code/41 - 50/44.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    printf("Name - AKSHAT VERMA\nSAP_ID-590026318\nCourse-BCA\nBatch-6");
+    printf("\n--------------------------------------\n");
+
+    int n;
+    float sum = 0.0;
+    printf("Enter number of terms: ");
+    scanf("%d", &n);
+
+    int num = 1, den = 2;
+    for(int i = 1; i <= n; i++) {
+        sum += (float)num / den;
+        num += 2;
+        den += 2;
+    }
+
+    printf("Approximate sum: %.2f", sum);
+    return 0;
+}
