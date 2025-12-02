a4ce4a50deda76436ca2722956b3798d9a63dc38
Akshat Verma <akshat157verma@gmail.com>
2025-12-02 15:45:53 +0530

    Create 104.c

---
+++ 100 Days of code/101 - 110/104.c
@@ -0,0 +1,34 @@
+/*
+Q104: Find integer x such that:
+sum(1…x) == sum(x…n)
+If none → print -1
+
+Sample:
+n = 8 → Output: 6
+*/
+
+#include <stdio.h>
+
+int main() {
+
+    printf("Name - AKSHAT VERMA\nSAP_ID-590026318\nCourse-BCA\nBatch-6");
+    printf("\n--------------------------------------\n");
+
+    int n;
+    scanf("%d", &n);
+
+    int total = n * (n + 1) / 2;
+
+    for (int x = 1; x <= n; x++) {
+        int left = x * (x + 1) / 2;
+        int right = total - (x - 1) * x / 2;
+
+        if (left == right) {
+            printf("%d", x);
+            return 0;
+        }
+    }
+
+    printf("-1");
+    return 0;
+}
