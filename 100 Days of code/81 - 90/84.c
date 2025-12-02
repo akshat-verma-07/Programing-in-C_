9a5e0b92c952726c1b627d4da40801fb7ca01807
Akshat Verma <akshat157verma@gmail.com>
2025-12-02 15:08:43 +0530

    Create 84.c

---
+++ 100 Days of code/81 - 90/84.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    printf("Name - AKSHAT VERMA\nSAP_ID-590026318\nCourse-BCA\nBatch-6");
+    printf("\n--------------------------------------\n");
+
+    char str[1000];
+    scanf("%s",str);
+
+    for(int i=0; str[i]; i++) {
+        if(str[i]>='a' && str[i]<='z')
+            str[i] = str[i] - 32;
+    }
+
+    printf("%s",str);
+    return 0;
+}
